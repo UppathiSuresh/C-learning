af225547675fe9d7b1b35e054ec64dbdef539a0c
UppathiSuresh <sureshuppathi@gmail.com>
2017-02-04 23:14:08 +0530

    added hello world pgm

---
+++ hello.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*
+            int a;
+            Console.WriteLine("enter the value of a is");
+           
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}",a);
+            Console.ReadLine();
+        */
+            Console.WriteLine("hello world");
+        }
+    }
+}
